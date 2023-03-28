91e4d90a5b3510693043c292ec323782abf6726f
Bubbl3Waffl3 <salahlfd0@gmail.com>
2023-03-28 09:34:20 +0200

    fixed navbar

--- src/Components/Navbar/Navbar.jsx
+++ src/Components/Navbar/Navbar.jsx
@@ -4,12 +4,10 @@ import Plus from '../../Assets/Img/Plus.png'
 import Hamburguer from '../../Assets/Img/Hamburguer.png'
 const Navbar = () => {
   return (   
-      <div className='navigation'>
-        <nav>
-          <img className='plusNavigation' src={Plus}/>
-          <img className='hamburguernavigation' src={Hamburguer}/>
-        </nav>      
-      </div>    
+      <nav className='navigation'>
+        <img className='plusNavigation' src={Plus}/>
+        <img className='hamburguernavigation' src={Hamburguer}/>
+      </nav>    
   );
 }
 
