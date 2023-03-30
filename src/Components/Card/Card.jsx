e89951826f967ece55d0b0a378145b3c843f3ca7
Ashur19 <iglesiasjota1911@gmail.com>
2023-03-30 10:20:26 +0200

    Details

--- src/Components/Card/Card.jsx
+++ src/Components/Card/Card.jsx
@@ -1,25 +1,20 @@
-import React, { useState } from 'react'
+import React from 'react'
 import { Navigate, useNavigate } from 'react-router-dom'
-import HeartButton from '../Heart/HeartButton'
 import './Card.css'
 
 
 
 const Card = ({id, title, url}) => {
-  let [toggle, setToggle] = useState("")
+  
   let navigate = useNavigate()
 
 
-  const like = () => {
-   toggle === "heart-active" ? setToggle("") : setToggle("heart-active")
-  }
   return (
     
     <div>
       <div className='cardContainer'>
         <div className='cardImage'>
             <img  src={url} alt='' />
-           <HeartButton/>
         </div>
         <h3 onClick={()=> {navigate("/Article", {state: {id}})}} className='cardText'>{title}</h3>
       </div>      
