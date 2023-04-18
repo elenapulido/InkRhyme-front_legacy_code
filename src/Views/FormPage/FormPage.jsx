774d2659b1fa3e473562d5171828c9af10658821
JavierBP <fjavierbresperez@yahoo.es>
2023-04-18 22:26:05 +0200

    changing footer

--- src/Views/FormPage/FormPage.jsx
+++ src/Views/FormPage/FormPage.jsx
@@ -1,17 +1,19 @@
 import React from 'react'
-import "./FormPage.css"
+// import "./FormPage.css"
 import Form from '../../Components/Form/Form'
 import Footer from '../../Components/Footer/Footer'
 import Header from '../../Components/Header/Header'
 import Navbar from '../../Components/Navbar/Navbar'
+import Footer2 from '../../Components/Footer/Footer2'
 
 function FormPage() {
   return (
     <div className='FormPage'>
-      <Header />
+      {/* <Header />
       <Navbar />
       <Form />
-      <Footer />
+      <Footer /> */}
+      <Footer2 />      
     </div>
   )
 }
