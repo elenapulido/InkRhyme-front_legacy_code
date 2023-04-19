fb85cf089a8f51059c89323f09a2ab61dd064066
elenapulido <119506074+elenapulido@users.noreply.github.com>
2023-04-19 09:34:30 +0200

    EditForm

--- src/Components/Article/Article.jsx
+++ src/Components/Article/Article.jsx
@@ -11,6 +11,7 @@ import Typography from '@mui/joy/Typography';
 import Button from '@mui/joy/Button';
 import Avatar from '@mui/joy/Avatar';
 import Box from '@mui/joy/Box';
+import EditForm from "../EditForm/EditForm";
 
 
 function Article() {
@@ -33,47 +34,54 @@ function Article() {
     }
   };
 
-  //   const handleEdit = (id) => {
-  //     const dataToEdit = data.find(d => d.id === id);
-  //     setEditingData(dataToEdit);
-  // }
+  const handleEdit = (id) => {
+    const dataToEdit = data.find(d => d.id === id);
+    setEditingData(dataToEdit);
+  }
 
   return (
-
-    <div>
-      <Card variant="outlined" orientation="horizontal" sx={{ width: 320, gap: 2, '&:hover': { boxShadow: 'md', borderColor: 'neutral.outlinedHoverBorder' }, }}>
-        <Box sx={{ display: 'flex', alignItems: 'center', pb: 1.5, gap: 1 }}>
-          <Box sx={{ position: 'relative', '&:before': { content: '""', position: 'absolute', top: 0, left: 0, bottom: 0, right: 0, m: '-2px', borderRadius: '50%', background: 'linear-gradient(45deg, #f09433 0%,#e6683c 25%,#dc2743 50%,#cc2366 75%,#bc1888 100%)', }, }} >
-            <Avatar size="sm" alt=" " sx={{ p: 0.5, border: '2px solid', borderColor: 'background.body' }} />
-          </Box>
-          <Typography fontWeight="lg">{data.author}</Typography>
-        </Box>
-        <AspectRatio ratio="1" sx={{ width: 90 }}>
-          <img src={data.url} loading="lazy" alt="" />
-        </AspectRatio>
-
+    <>
+      {editingData ? (
         <div>
-         
-          <Typography level="h2" fontSize="lg" id="card-description" mb={0.5}>
-            {data.title}
-          </Typography>
-
-          <Typography level="h2" fontSize="lg" id="card-description" mb={0.5}>
-            {data.genre}
-          </Typography>
-
-          <Typography fontSize="sm" aria-describedby="card-description" mb={1}>
-            <Link overlay underline="none" href="#interactive-card" sx={{ color: 'text.tertiary' }} >
-              {data.poem}
-            </Link>
-          </Typography>
-
-          <Button variant="outlined" color="neutral" onClick={handleDelete} size="sm">Borrar</Button>
-          <Button variant="outlined" color="neutral" onClick={() => { navigate("/Form", { state: { id: idInState } }) }} size="sm">Editar</Button>
-          <HeartButton />
+          <EditForm data={editingData} onSubmit={handleEdit} onCancel={handleEdit} />
         </div>
-      </Card>
-    </div>
+      ) : (
+        <div>
+          <Card variant="outlined" orientation="horizontal" sx={{ width: 320, gap: 2, '&:hover': { boxShadow: 'md', borderColor: 'neutral.outlinedHoverBorder' }, }}>
+            <Box sx={{ display: 'flex', alignItems: 'center', pb: 1.5, gap: 1 }}>
+              <Box sx={{ position: 'relative', '&:before': { content: '""', position: 'absolute', top: 0, left: 0, bottom: 0, right: 0, m: '-2px', borderRadius: '50%', background: 'linear-gradient(45deg, #f09433 0%,#e6683c 25%,#dc2743 50%,#cc2366 75%,#bc1888 100%)', }, }} >
+                <Avatar size="sm" alt=" " sx={{ p: 0.5, border: '2px solid', borderColor: 'background.body' }} />
+              </Box>
+              <Typography fontWeight="lg">{data.author}</Typography>
+            </Box>
+            <AspectRatio ratio="1" sx={{ width: 90 }}>
+              <img src={data.url} loading="lazy" alt="" />
+            </AspectRatio>
+
+            <div>
+
+              <Typography level="h2" fontSize="lg" id="card-description" mb={0.5}>
+                {data.title}
+              </Typography>
+
+              <Typography level="h2" fontSize="lg" id="card-description" mb={0.5}>
+                {data.genre}
+              </Typography>
+
+              <Typography fontSize="sm" aria-describedby="card-description" mb={1}>
+                <Link overlay underline="none" href="#interactive-card" sx={{ color: 'text.tertiary' }} >
+                  {data.poem}
+                </Link>
+              </Typography>
+
+              <Button variant="outlined" color="neutral" onClick={handleDelete} size="sm">Borrar</Button>
+              <Button variant="outlined" color="neutral" onClick={() => handleEdit(data.id)} size="sm">Editar</Button>
+              <HeartButton />
+            </div>
+          </Card>
+        </div>
+      )}
+    </>
   );
 }
 
--- src/Components/EditForm/EditForm.jsx
+++ src/Components/EditForm/EditForm.jsx
@@ -1,69 +1,69 @@
-// import React, { useState } from "react";
-// import { Button, Form, TextArea } from 'semantic-ui-react';
-// import HTTPService from "../../Services/HTTPService";
+import React, { useState } from "react";
+import { Button, Form, TextArea } from 'semantic-ui-react';
+import HTTPService from "../../Services/HTTPService";
 
-// const EditForm = ({ data, setData, onCancel }) => {
-//     const [editedData, setEditedData] = useState({ ...data });
+const EditForm = ({ data, setData, onCancel }) => {
+    const [editedData, setEditedData] = useState({ ...data });
 
-//     const handleInputChange = (event) => {
-//         const { name, value } = event.target;
-//         setEditedData({ ...editedData, [name]: value });
+    const handleInputChange = (event) => {
+        const { name, value } = event.target;
+        setEditedData({ ...editedData, [name]: value });
 
-//     };
-
-
-//     const handleSubmit = async (event) => {
-//         event.preventDefault();
-//         try {
-//             const updatedData = await HTTPService().updateData(data.id, editedData);
-//             setData(updatedData);
-//         } catch (error) {
-//             console.log(error);
-
-//         }
-//     };
+    };
 
 
-//     return (
-//         <div className='main-form'>
-//             <h2>EDITAR</h2>
-//             <Form className='create-form' onSubmit={handleSubmit}>
-//                 <Form.Field>
-//                     <label>TÍTULO</label>
-//                     <input value={editedData.name} onChange={handleInputChange} type="text" id="name" name="name" />
-//                 </Form.Field>
-//                 <Form.Field>
-//                     <label>DESCRIPCIÓN</label>
-//                     <TextArea value={editedData.description} onChange={handleInputChange}
-//                         rows={7} id="description" name="description" />
-//                 </Form.Field>
-//                 <Form.Field label='TIPO' id="theme" control='select' onChange={handleInputChange}
-//                     value={editedData.theme} >
-//                     <label>TIPO</label>
-//                     <input type="text" />
-//                     <option value='Mitos y leyendas'>Mitos y leyendas</option>
-//                     <option value='Avistamientos ovnis'>Avistamientos ovnis</option>
-//                     <option value='Experiencias paranormales'>Experiencias paranormales</option>
-//                 </Form.Field>
-//                 <Form.Field>
-//                     <label>LOCALIZACIÓN</label>
-//                     <input value={editedData.location} onChange={handleInputChange} type="text" id="location" name="location" />
-//                 </Form.Field>
-//                 <Form.Field>
-//                     <label>IMAGEN</label>
-//                     <input value={editedData.url} onChange={handleInputChange} type="text" id="exampleFormControlInput2" name="url" />
-//                 </Form.Field>
-//                 <Form.Field>
-//                     <label for="myfile">CARGAR IMÁGENES</label>
-//                     <input type="file" id='myfile' name='myfile'></input>
-//                 </Form.Field>
-//                 <div>
-//                     <Button type="submit" content='Enviar' icon='like'></Button>
-//                     <Button type="submit" content='Volver' icon='cancel' href='/leyendas' onClick={onCancel}></Button>
-//                 </div>
-//             </Form>
-//         </div>
-//     );
-// };
+    const handleSubmit = async (event) => {
+        event.preventDefault();
+        try {
+            const updatedData = await HTTPService().ApiPutService(data.id, editedData);
+            setData(updatedData);
+        } catch (error) {
+            console.log(error);
 
-// export default EditForm;
+        }
+    };
+
+
+    return (
+        <div className='main-form'>
+            <h2>EDITAR</h2>
+            <Form className='create-form' onSubmit={handleSubmit}>
+                <Form.Field>
+                    <label>TÍTULO</label>
+                    <input value={editedData.title} onChange={handleInputChange} type="text" id="title" name="title" />
+                </Form.Field>
+                <Form.Field>
+                    <label>AUTOR</label>
+                    <TextArea value={editedData.author} onChange={handleInputChange}
+                        rows={7} id="author" name="author" />
+                </Form.Field>
+                <Form.Field label='TIPO' id="theme" control='select' onChange={handleInputChange}
+                    value={editedData.genre} >
+                    <label>TIPO</label>
+                    <input type="text" />
+                    <option value='Lírica'>Lírica</option>
+                    <option value='Épica'>Épica</option>
+                    <option value='Dramática'>Dramática</option>
+                    <option value='Romántica'>Romántica</option>
+                </Form.Field>
+                <Form.Field>
+                    <label>POEMA</label>
+                    {/* <input value={editedData.poem} onChange={handleInputChange} type="text" id="location" name="location" /> */}
+                    <textarea rows="8" type="text" value={editedData.poem} onChange={handleInputChange} name="poem" id="poem" required />
+                </Form.Field>
+                <Form.Field>
+                    <label>IMAGEN</label>
+                    {/* <input value={editedData.url} onChange={handleInputChange} type="text" id="exampleFormControlInput2" name="url" /> */}
+                    <input type="url" id="url" name="url" value={editedData.url} onChange={handleInputChange} autoComplete="off" placeholder="Url de la Imagén" required pattern="https?://.+" />
+                </Form.Field>
+                
+                <div>
+                    <Button type="submit" content='Enviar' icon='like'></Button>
+                    <Button type="submit" content='Volver' icon='cancel' href='/leyendas' onClick={onCancel}></Button>
+                </div>
+            </Form>
+        </div>
+    );
+};
+
+export default EditForm;
